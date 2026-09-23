17e640af68d6f9a2b5ba57680ad4fdc77c9f9919
potuzhnyj <197530600+potuzhnyj@users.noreply.github.com>
2026-09-23 19:52:14 +0200

    language update

---
+++ scripts/lang.js
@@ -0,0 +1,25 @@
+let translations;
+
+async function load() {
+  const response = await fetch("../data/languages.json");
+  translations = await response.json();
+  const savedLanguage = localStorage.getItem("language") || "en";
+
+  languageSelect.value = savedLanguage;
+  setLanguage(savedLanguage);
+}
+
+function setLanguage(lang) {
+  document.querySelectorAll("[data-i18n]").forEach((e) => {
+    const key = e.dataset.i18n;
+    e.textContent = translations[lang][key];
+  });
+
+  localStorage.setItem("language", lang);
+}
+
+languageSelect.addEventListener("change", () => {
+  setLanguage(languageSelect.value);
+});
+
+load();
